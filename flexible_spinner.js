0c835c6906bee07ae389bcf0b51c1f2727cf55f5
titovanton <mail@titovanton.com>
2014-10-11 01:00:34 +0600

    refactoring

--- flexible_spinner.js
+++ flexible_spinner.js
@@ -1,66 +1,125 @@
 (function($){
-    $.fn.flexible_spinner = function (init) {
-        var time_out_id       = null
-        var after_time_out_id = null
-        var interval_id       = null
-        var target            = this
-        var settings = {
-            inc              : 'inc-selector',
-            dec              : 'dec-selector',
-            sticky_key       : true,
-            sticky_key_delay : 500,
-            change_speed     : 50,
-            after_done       : 1000,
+    $.fn.flexibleSpinner = function (jsInit) {
+        var doDec, doInc
 
-            inc_handler: function(){
-                var value = $(target).val()
-                value = value * 1 + 1
-                $(target).val(value)
-            },
-            dec_handler: function(){
-                var value = $(target).val()
-                if (value > 1) value = value * 1 - 1
-                $(target).val(value)
+        doDec = function() {
+            var value = $(this).val()
+            if (value > 1) value = value*1 - 1
+            $(this).val(value)
+        }
+
+        doInc = function() {
+            var value = $(this).val()
+            value = value*1 + 1
+            $(this).val(value)
+        }
+
+        function init($spinner) {
+
+            // default init
+            var settings = {
+
+                    // could be init from data-<name> attrs
+                    inc: '.inc-spinner',
+                    dec: '.dec-spinner',
+                    stickyKeyDelay: 500,
+                    stickyChangeSpeed: 50,
+
+                    // delay, after which an afterDone.flexibleSpinner event will happen
+                    afterDone: 1000,
+
+                    // could be init from javascript only
+                    doInc: doInc,
+                    doDec: doDec
+                }
+
+            // HTML attr init
+            var except = ['doInc', 'doDec']
+            for (var k in settings) {
+                if ($.inArray(k, except) === -1) {
+                    var attr = 'data-' + k
+                    if (typeof($spinner.attr(attr)) != 'undefined') {
+                        settings[k] = $spinner.attr(attr)
+                    }
+                }
+            }
+
+            // init from js
+            $.extend(settings, jsInit)
+
+            // stor the data for future needs
+            $spinner.data('flexibleSpinnerSettings', settings)
+            return settings
+        }
+
+        function incMouseDownHandler() {
+            var $spinner = $(this),
+                settings = $spinner.data('flexibleSpinnerSettings'),
+                timeOutId,
+                intervalId
+            $spinner.trigger('incMouseDown.flexibleSpinner')
+            if (settings.stickyKeyDelay) {
+                timeOutId = setTimeout(function(){
+                    intervalId = setInterval(function(){
+                        $spinner.trigger('incMouseDown.flexibleSpinner')
+                    }, settings.stickyChangeSpeed)
+                    $spinner.data('intervalId', intervalId)
+                }, settings.stickyKeyDelay)
+                $spinner.data('timeOutId', timeOutId)
             }
         }
-        $.extend(settings, init)
-        $(settings.inc).mousedown(function(){
-            target.trigger('flexible-spinner-inc')
-            if (settings.sticky_key) {
-                time_out_id = setTimeout(function(){
-                    interval_id = setInterval(function(){
-                        target.trigger('flexible-spinner-inc')
-                    }, settings.change_speed)
-                }, settings.sticky_key_delay)
-            }
-        })
-        $(settings.inc).mouseup(function(){
-            target.trigger('flexible-spinner-done')
-        })
-        $(settings.dec).mousedown(function(){
-            target.trigger('flexible-spinner-dec')
-            if (settings.sticky_key) {
-                time_out_id = setTimeout(function(){
-                    interval_id = setInterval(function(){
-                        target.trigger('flexible-spinner-dec')
-                    }, settings.change_speed)
-                }, settings.sticky_key_delay)
-            }
-        })
-        $(settings.dec).mouseup(function(){
-            target.trigger('flexible-spinner-done')
-        })
-        target.bind('flexible-spinner-inc', settings.inc_handler)
-        target.bind('flexible-spinner-dec', settings.dec_handler)
-        target.bind('flexible-spinner-done', function(){
-            if (time_out_id) clearTimeout(time_out_id)
-            if (after_time_out_id) clearTimeout(after_time_out_id)
-            if (interval_id) clearInterval(interval_id)
-            after_time_out_id = setTimeout(function(){
-                target.trigger('flexible-spinner-after-done')
-            }, settings.after_done)
-        })
 
-        return target
+        function decMousedownHandler() {
+            var $spinner = $(this),
+                settings = $spinner.data('flexibleSpinnerSettings'),
+                timeOutId,
+                intervalId
+            $spinner.trigger('decMouseDown.flexibleSpinner')
+            if (settings.stickyKeyDelay) {
+                timeOutId = setTimeout(function(){
+                    intervalId = setInterval(function(){
+                        $spinner.trigger('decMouseDown.flexibleSpinner')
+                    }, settings.stickyChangeSpeed)
+                    $spinner.data('intervalId', intervalId)
+                }, settings.stickyKeyDelay)
+                $spinner.data('timeOutId', timeOutId)
+            }
+        }
+
+        function mouseUpHandler() {
+            var $spinner = $(this),
+                settings = $spinner.data('flexibleSpinnerSettings')
+            $spinner.trigger('mouseUp.flexibleSpinner')
+        }
+
+        return this.each(function() {
+            var $spinner = $(this), // an input with class .flexible-spinner
+                settings = init($spinner)
+
+            $(settings.inc).mousedown(incMouseDownHandler)
+            $(settings.dec).mousedown(decMousedownHandler)
+            $(settings.inc).mouseup(mouseUpHandler)
+            $(settings.dec).mouseup(mouseUpHandler)
+
+            $spinner.bind('incMouseDown.flexibleSpinner', settings.doInc)
+            $spinner.bind('decMouseDown.flexibleSpinner', settings.doDec)
+            $spinner.bind('mouseUp.flexibleSpinner', function(){
+                var $spinner = $(this),
+                    timeOutId = $spinner.data('timeOutId'),
+                    intervalId = $spinner.data('intervalId'),
+                    afterTimeOutId = $spinner.data('afterTimeOutId')
+                if (timeOutId != 'undefined') clearTimeout(timeOutId)
+                if (afterTimeOutId != 'undefined') clearTimeout(afterTimeOutId)
+                if (intervalId != 'undefined') clearInterval(intervalId)
+                var afterTimeOutId = setTimeout(function(){
+                    $spinner.trigger('afterDone.flexibleSpinner')
+                }, settings.afterDone)
+                $spinner.data('afterTimeOutId', afterTimeOutId)
+            })
+        })
     }
+
+    $(function(){
+        $('.flexible-spinner').flexibleSpinner()
+    })
 })(jQuery);
